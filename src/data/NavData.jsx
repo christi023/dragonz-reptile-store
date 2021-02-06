ac5f24c3844505f709bbb3efb5010cb373dac96a
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-02-06 18:41:24 +0100

    This holds the navabar data

---
+++ src/data/NavData.jsx
@@ -0,0 +1,27 @@
+export const navData = [
+  { title: 'Welcome', link: '/' },
+  {
+    title: 'News',
+    link: '/news',
+  },
+  {
+    title: 'About',
+    link: '/about',
+  },
+  {
+    title: 'Packages',
+    link: '/packages',
+  },
+  {
+    title: 'Collection',
+    link: '/collection',
+  },
+  {
+    title: 'Blog',
+    link: '/blog',
+  },
+  {
+    title: 'Contact Us ',
+    link: '/contact',
+  },
+];
