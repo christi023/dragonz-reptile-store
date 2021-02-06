c818582490d40cd12e763f9b051f076002ddb40b
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-02-06 18:40:45 +0100

    This holds the data for carousel

---
+++ src/data/CarrouselData.jsx
@@ -0,0 +1,36 @@
+import img1 from '../assets/snakes.jpg';
+import img2 from '../assets/lizards.jpg';
+import img3 from '../assets/turtles.jpg';
+import img4 from '../assets/frogs.jpg';
+
+export const ObjOne = {
+  img: img1,
+  h2: 'SNAKES',
+  p:
+    'There are more than 3,000 species of snakes on the planet and they’re found everywhere except in Antarctica, Iceland, Ireland, Greenland, and New Zealand.',
+  button: 'MORE',
+};
+
+export const ObjTwo = {
+  img: img2,
+  h2: 'LIZARDS',
+  p:
+    'Moving among the trees in the jungles of Southeast Asia is an essential task—for escaping danger, attracting mates, and finding meals.',
+  button: 'MORE',
+};
+
+export const ObjThree = {
+  img: img3,
+  h2: 'TURTLES',
+  p:
+    'The largest of all hard-shelled turtles, loggerheads are named for their massive heads and strong jaws (leatherbacks are bigger but have soft shells).',
+  button: 'MORE',
+};
+
+export const ObjFour = {
+  img: img4,
+  h2: 'FROGS',
+  p:
+    'The Togo slippery frog is found solely along the border of Ghana and Togo, in the lush highlands of the Togo-Volta region.',
+  button: 'MORE',
+};
