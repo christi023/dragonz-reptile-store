c32ee6c71871d29e211e726555fefdaaddb4f4e3
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-02-06 14:54:50 +0100

    responsive navbar

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -1,25 +1,40 @@
 import React from 'react';
 import { navData } from '../../data/NavData';
 import { Link } from 'react-router-dom';
-import img from '../../assets/logo.png';
-
+import logo from '../../assets/logo.png';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 // styles
 import './Navbar.css';
 
-export default function Navbar() {
+export default function NavBar() {
   return (
-    <div className="nav">
-      <Link to="/">
-        <img src={img} alt="" />
-      </Link>
-      <div className="menu-bars"></div>
-      <div className="nav-menu">
-        {navData.map((item, i) => (
-          <Link to={item.links} key={i} className="nav-menu-links">
-            {item.title}
-          </Link>
-        ))}
-      </div>
-    </div>
+    <>
+      <Navbar bg="light" variant="dark" expand="lg" collapseOnSelect className="navbar-fixed-top">
+        <Container>
+          <Navbar.Brand>
+            <Link to="/">
+              <img
+                src={logo}
+                alt="logo"
+                style={{ maxHeight: '110px', marginTop: '-15px' }}
+                className="hidden-xs"
+              />
+            </Link>
+          </Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              {navData.map((item, i) => (
+                <Link to={item.links} key={i} className="nav-menu-links">
+                  {item.title}
+                </Link>
+              ))}
+              <br />
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 }
