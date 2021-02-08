213dd19a0c2ec55e1a55b58258bdee8d22a40531
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-02-08 20:01:59 +0100

    About data details

---
+++ src/data/AboutData.jsx
@@ -0,0 +1,14 @@
+export const objOne = {
+  h3: '25 Years Reputation',
+  p: 'Our reputation depends on our ability to deliver healthy animals to our customers.',
+};
+
+export const objTwo = {
+  h3: 'Quick & Efficient',
+  p:
+    'You are able to order snakes, frogs, turtles, lizards, crickets, feeders, cages and supplies 24/7 from the comfort of your computer without waiting on an fax or speaking to anyone on the phone.',
+};
+export const objThree = {
+  h3: 'Full Integrity',
+  p: 'Our sales department is adding new items to the product list weekly. ',
+};
