3f080f118894554a426a94f1285fb878a9174249
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-02-09 12:33:06 +0100

    Welcome details

---
+++ src/data/WelcomeData.jsx
@@ -0,0 +1,15 @@
+import video1 from '../assets/turtle.mp4';
+import video2 from '../assets/snake.mp4';
+import video3 from '../assets/lizard.mp4';
+
+export const objOne = {
+  src: video1,
+};
+
+export const objTwo = {
+  src: video2,
+};
+
+export const objThree = {
+  src: video3,
+};
