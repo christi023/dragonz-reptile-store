ff5a73988ed50caacb9f4327adab59d91606df26
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-02-10 18:52:15 +0100

    Packages details

---
+++ src/data/PackageData.jsx
@@ -0,0 +1,41 @@
+import croc from '../assets/crocodile.jpg';
+
+export const objOne = {
+  imgSrc: croc,
+  h3: 'Crocodile',
+  li: '5 Suspendisse Porttitor',
+  p:
+    "Some quick example text to build on the card title and make up the bulk of the card's content.",
+  price: '88.99',
+  link: '/croc',
+};
+
+export const objTwo = {
+  imgSrc: croc,
+  h3: 'Crocodile',
+  li: '5 Suspendisse Porttitor',
+  p:
+    "Some quick example text to build on the card title and make up the bulk of the card's content.",
+  price: '88.99',
+  link: '/croc',
+};
+
+export const objThree = {
+  imgSrc: croc,
+  h3: 'Crocodile',
+  li: '5 Suspendisse Porttitor',
+  p:
+    "Some quick example text to build on the card title and make up the bulk of the card's content.",
+  price: '88.99',
+  link: '/croc',
+};
+
+export const objFour = {
+  imgSrc: croc,
+  h3: 'Crocodile',
+  li: '5 Suspendisse Porttitor',
+  p:
+    "Some quick example text to build on the card title and make up the bulk of the card's content.",
+  price: '88.99',
+  link: '/croc',
+};
