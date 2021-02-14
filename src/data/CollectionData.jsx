22473faf671c0dbefe8c1a22c571b668d97ab9f6
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-02-14 17:40:16 +0100

    Holds the collection details

---
+++ src/data/CollectionData.jsx
@@ -0,0 +1,57 @@
+import snakes from '../assets/snakes.jpg';
+import viper from '../assets/bush-viper.jpg';
+import dragon from '../assets/bearded-dragon.jpg';
+import basilisk from '../assets/basilisk.jpg';
+import chameleon from '../assets/chameleon.jpg';
+import leopard from '../assets/leopard-gecko.jpg';
+import savannah from '../assets/savannah.jpg';
+import pacman from '../assets/pacman.jpg';
+
+export const objOne = {
+  imgSrc: snakes,
+  h3: 'Green Anaconda',
+  p: 'Maria Phillip',
+  link: '/more details',
+};
+export const objTwo = {
+  imgSrc: viper,
+  h3: 'Bush Viper',
+  p: 'Maria Phillip',
+  link: '/more details',
+};
+export const objThree = {
+  imgSrc: dragon,
+  h3: 'Bearded Dragon',
+  p: 'Maria Phillip',
+  link: '/more details',
+};
+export const objFour = {
+  imgSrc: basilisk,
+  h3: 'Basilisk Lizard',
+  p: 'Maria Phillip',
+  link: '/more details',
+};
+export const objFive = {
+  imgSrc: chameleon,
+  h3: 'Chameleon',
+  p: 'Maria Phillip',
+  link: '/more details',
+};
+export const objSix = {
+  imgSrc: leopard,
+  h3: 'Leopard Gecko ',
+  p: 'Maria Phillip',
+  link: '/more details',
+};
+export const objSeven = {
+  imgSrc: savannah,
+  h3: 'Savannah monitor ',
+  p: 'Maria Phillip',
+  link: '/more details',
+};
+export const objEight = {
+  imgSrc: pacman,
+  h3: 'Pacman Frog ',
+  p: 'Maria Phillip',
+  link: '/more details',
+};
