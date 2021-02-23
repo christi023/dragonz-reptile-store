8c603dd169de5d98bac9f61eee1a3f56fbf65c99
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-02-23 14:36:33 +0100

    Collection and Blog Routes added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// Components
+import NavBar from './Components/NavBar/NavBar';
+import Home from './Components/Home/Home';
+// Pages
+import News from './Pages/News/News';
+import About from './Pages/About/About';
+import Packages from './Pages/Packages/Packages';
+import Collection from './Pages/Collection/Collection';
+import Blog from './Pages/Blog/Blog';
+
+// style
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/news" component={News} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/packages" component={Packages} />
+        <Route exact path="/collection" component={Collection} />
+        <Route exact path="/blog" component={Blog} />
+      </Switch>
+    </Router>
   );
 }
-
-export default App;
