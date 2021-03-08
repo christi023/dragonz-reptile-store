5e58fd90eb9cb8fed779a5be3eb5588a63b1e8d3
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-03-08 20:10:33 +0100

    Contact form and styling created for website

---
+++ src/Components/Contact/Login.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import Footer from '../Footer/Footer';
+// style
+import './Register_Login.css';
+
+export default function Register() {
+  const { register, handleSubmit, errors } = useForm();
+
+  const onSubmit = (data, e) => {
+    e.target.reset(); // reset after form submit
+    console.log(data);
+    alert('You are now logged in');
+  };
+
+  return (
+    <>
+      <div className="form-display">
+        <form className="" onSubmit={handleSubmit(onSubmit)}>
+          <label className="form-label">Email</label>
+          {errors.email && <span>This is required</span>}
+          <input
+            type="email"
+            name="email"
+            placeholder="info@example.com"
+            ref={register({ required: true })}
+          />
+
+          <label className="form-label">Password</label>
+          {errors.password && <span>This is required</span>}
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter password"
+            ref={register({ required: true })}
+          />
+          {errors.password && <span>This is required</span>}
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      <br />
+      <br />
+      <Footer />
+    </>
+  );
+}
