6f67648bd3452e01a31ac45d2d75ca2516ce5966
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-03-17 20:56:54 +0100

    added to package data

--- src/data/PackageData.jsx
+++ src/data/PackageData.jsx
@@ -1,41 +1,61 @@
-import croc from '../assets/crocodile.jpg';
+import boa from '../assets/rainbow-boa.jpg';
+import greenPython from '../assets/green-python.jpeg';
+import agama from '../assets/agama.jpg';
+import panther from '../assets/panther.jpeg';
+import basilisk from '../assets/green-basilisk.jpg';
+import milkSnake from '../assets/milkSnake.jpg';
 
 export const objOne = {
-  imgSrc: croc,
-  h3: 'Crocodile',
+  imgSrc: boa,
+  h3: 'Brazilian Rainbow Boa',
+  h1: '',
   li: '5 Suspendisse Porttitor',
-  p:
-    "Some quick example text to build on the card title and make up the bulk of the card's content.",
+  p: 'Epicrates Cenchria.',
   price: '88.99',
-  link: '/croc',
+  link: '/blog',
 };
 
 export const objTwo = {
-  imgSrc: croc,
-  h3: 'Crocodile',
+  imgSrc: greenPython,
+  h3: 'Green Tree Python',
   li: '5 Suspendisse Porttitor',
-  p:
-    "Some quick example text to build on the card title and make up the bulk of the card's content.",
+  p: 'Morelia Viridis.',
+
   price: '88.99',
-  link: '/croc',
+  link: '/blog',
 };
 
 export const objThree = {
-  imgSrc: croc,
-  h3: 'Crocodile',
+  imgSrc: agama,
+  h3: 'Common Agama Lizard',
   li: '5 Suspendisse Porttitor',
-  p:
-    "Some quick example text to build on the card title and make up the bulk of the card's content.",
+  p: 'Agama Agama.',
   price: '88.99',
-  link: '/croc',
+  link: '/blog',
 };
 
 export const objFour = {
-  imgSrc: croc,
-  h3: 'Crocodile',
+  imgSrc: panther,
+  h3: 'Panther Chameleon',
   li: '5 Suspendisse Porttitor',
-  p:
-    "Some quick example text to build on the card title and make up the bulk of the card's content.",
+  p: 'Furcifer Paradalis.',
   price: '88.99',
-  link: '/croc',
+  link: '/blog',
+};
+
+export const objFive = {
+  imgSrc: basilisk,
+  h3: 'Basilisk',
+  li: '5 Suspendisse Porttitor',
+  p: 'Basiliscus Plumifrons.',
+  price: '88.99',
+  link: '/blog',
+};
+export const objSix = {
+  imgSrc: milkSnake,
+  h3: 'Sinaloan Milk Snake',
+  li: '5 Suspendisse Porttitor',
+  p: 'Lampropeltis Triangulum.',
+  price: '88.99',
+  link: '/blog',
 };
